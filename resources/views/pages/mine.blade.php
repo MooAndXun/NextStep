3f7b4758ed8779fa596d8da1d18e27607a5287f8
chenmuen <593827475@qq.com>
2016-11-29 18:34:20 +0800

    Update mine blade

--- resources/views/pages/mine.blade.php
+++ resources/views/pages/mine.blade.php
@@ -12,7 +12,7 @@
           <i class="material-icons large">directions_run</i>
         </div>
         <div class="card-content">
-          <p class="center">你今天共走了 8000 步</p>
+          <p class="center">你今天共走了 {{ $steps }} 步</p>
         </div>
       </div>
       <div class="card sport-today-card">
@@ -25,7 +25,7 @@
         </div>
 
         <div class="card-content">
-          <p class="center">你昨晚共睡了7个小时</p>
+          <p class="center">你昨晚共睡了{{ $sleep_hour }}个小时</p>
         </div>
       </div>
     </div>
@@ -34,7 +34,7 @@
   <div class="card list-card rank-card">
     <div class="card-content">
       <div class="card-title">步数排行</div>
-      <p class="center">今天你的运动步数排在第 <b>1</b> 名</p>
+      <p class="center">今天你的运动步数排在第 <b>{{$rank}}</b> 名</p>
     </div>
     <ul class="collection">
       <li class="collection-item collection-header">
@@ -42,30 +42,16 @@
         <div class="collection-col collection-col-2 count-col">步数</div>
         <div class="collection-col rank-col">排名</div>
       </li>
+      @foreach($user_ranks as $index=>$user_rank)
       <li class="collection-item">
         <div class="collection-col collection-col-2 user-col">
-          <img src="/img/person-1.jpg" alt="" class="avatar avatar-small">
-          <p>Arron Hunt</p>
+          <img src="{{$user_rank->avatar_img}}" alt="" class="avatar avatar-small">
+          <p>{{$user_rank->nick_name}}</p>
         </div>
-        <div class="collection-col collection-col-2 count-col">20010</div>
-        <div class="collection-col rank-col">1</div>
+          <div class="collection-col collection-col-2 count-col">{{$user_rank->steps}}</div>
+        <div class="collection-col rank-col">{{$index+1}}</div>
       </li>
-      <li class="collection-item">
-        <div class="collection-col collection-col-2 user-col">
-          <img src="/img/person-2.jpg" alt="" class="avatar avatar-small">
-          <p>Arron Hunt</p>
-        </div>
-        <div class="collection-col collection-col-2 count-col">12210</div>
-        <div class="collection-col rank-col">2</div>
-      </li>
-      <li class="collection-item">
-        <div class="collection-col collection-col-2 user-col">
-          <img src="/img/person-3.jpg" alt="" class="avatar avatar-small">
-          <p>Arron Hunt</p>
-        </div>
-        <div class="collection-col collection-col-2 count-col">8000</div>
-        <div class="collection-col rank-col">3</div>
-      </li>
+      @endforeach
     </ul>
   </div>
 @stop
