01d7d68c09330f276ee43140714c6300987af13f
chenmuen <593827475@qq.com>
2016-11-29 15:14:24 +0800

    Update page location

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
     public function index()
     {
 //        return view('home')->withUsers(\App\Models\User::all());
-        return view('mine');
+        return view('pages.mine');
     }
 }
