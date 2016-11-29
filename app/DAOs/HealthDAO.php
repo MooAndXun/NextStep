2cc75b309bc260b0feeaec1359aa34f82503f85b
xun <131250049@smail.nju.edu.cn>
2016-11-29 21:50:41 +0800

    change HomeController and add HealthDAO

---
+++ app/DAOs/HealthDAO.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 小春
+ * Date: 2016/11/29
+ * Time: 21:15
+ */
+
+namespace App\DAOs;
+
+use DB;
+
+
+class HealthDAO
+{
+    public function today_step_rank($username,$date){
+        $users_data = DB::select('SELECT nick_name,avatar as avatar_img,step.steps as steps FROM (SELECT * FROM follow WHERE follower_username = :username) follow
+                                    JOIN user following ON follow.following_username = following.username
+                                    JOIN step ON step.username = following.username
+                                    WHERE step."date" = :date
+                                  ORDER BY step.steps LIMIT 10', [':username'=>$username,':date'=>$date]);
+        return $users_data;
+    }
+
+    public function user_step_rank($username,$step,$date){
+        $rank = DB::select('SELECT count(*)+1 AS rank FROM (SELECT * FROM follow WHERE follower_username = :username) follow
+                                    JOIN user following ON follow.following_username = following.username
+                                    JOIN step ON step.username = following.username
+                                    WHERE step."date" = :date AND step.steps > :step
+                                  ORDER BY step.steps', [':username'=>$username,':step'=>$step,':date'=>$date]);
+        return $rank;
+    }
+
+
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,35 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DateTime;
 
 use App\Models\User;
+use App\DAOs\HealthDAO;
 
 class HomeController extends Controller
 {
+    protected $healthDAO;
+
+    public function __construct(HealthDAO $healthDAO)
+    {
+        $this->$healthDAO = $healthDAO;
+    }
 
     public function mine(Request $request)
     {
-        $user = User::find($request->session()->get('username'));
+//        $user = User::find($request->session()->get('username'));
+        $user = User::find('test');
         $date=new DateTime();
         $result = $date->format('Y-m-d');
         $steps = $user->steps()->where('date',$result)->first();
         $sleep = $user->sleep()->where('date',$result)->first();
-
+        $sleep_hour = ($sleep->sleep_minutes)/60;
+        $users_data = $this->healthDAO->today_step_rank($user->username,$date);
+        $rank = $this->healthDAO->user_step_rank($user->username,$steps->steps,$date);
+        foreach($users_data as $data){
+            //TODO
+        }
+//        print_r ($users_data);
         return view('pages.mine')->with([
-            'steps'=>100,
-            'sleep_hour'=>9,
+            'steps'=>$steps->steps,
+            'sleep_hour'=>$sleep_hour,
             'rank'=>1,
-            'user_ranks'=>[
-                ['avatar_img'=>'/img/person-1.jpg',
-                    'nick_name'=>'test',
-                    'steps'=>8000],
-                ['avatar_img'=>'/img/person-1.jpg',
-                    'nick_name'=>'test',
-                    'steps'=>8000],
-                ['avatar_img'=>'/img/person-1.jpg',
-                    'nick_name'=>'test',
-                    'steps'=>8000]
-            ]
+            'user_ranks'=>$users_data
         ]);
     }
 }
