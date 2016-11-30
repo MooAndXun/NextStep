d157178ada140e47a0b282c18abdf45e08808fe5
chenmuen <593827475@qq.com>
2016-12-01 01:05:37 +0800

    Add some view shared data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Config\Constant;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // View 共享数据
+        view()->share(['current_user'=>session('user')]);
     }
 
     /**
